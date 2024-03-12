70f80e9f2a44e83fa210f1603fb4a92547262c63
Izzatilla <112051978+1zzat1lla-Fayzullayev@users.noreply.github.com>
2024-03-12 06:56:42 +0500

    change

--- src/shared/modal/ImageModal.jsx
+++ src/shared/modal/ImageModal.jsx
@@ -9,7 +9,7 @@ function ImageModal({
   return (
     <>
       <dialog id="my_modal_3" className="modal">
-        <div className="modal-box">
+        <div className="modal-box flex flex-col gap-3">
           <form method="dialog">
             <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
               ✕
@@ -17,7 +17,14 @@ function ImageModal({
           </form>
           <h3 className="font-bold text-lg">Rasm yuborish uchun!</h3>
           <div>
-            <input type="file" onChange={handleFileChange} />
+            <div className="grid w-full max-w-xs items-center gap-[10px]">
+              <input
+                id="picture"
+                type="file"
+                className="flex h-10 w-full rounded-md border border-[#191e24] px-3 py-2 text-sm text-gray-400 file:border-0 file:bg-transparent file:text-gray-600 file:text-sm file:font-medium"
+                onChange={handleFileChange}
+              />
+            </div>
             {selectedImage && (
               <img
                 src={URL.createObjectURL(selectedImage)}
